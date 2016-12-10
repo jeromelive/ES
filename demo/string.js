368fc9079f72c7c6cd5db7cb0a7c8dac3fe6cf65
jeromelive <jeromelive@163.com>
2016-12-10 11:08:34 +0800

    --常用字符串--

--- demo/string.js
+++ demo/string.js
@@ -12,9 +12,35 @@ let longString = "This is a very long string which needs \
 				otherwise my code is unreadable.";
 console.log(longString);
 console.log(longString.length);
-console.log(longString.charAt(2));
+
+var hottext = 'MDN';
+var url = 'https://developer.mozilla.org/';
 
 console.log(String.fromCharCode(65, 66, 67));
+console.log(longString.charAt(2));
+console.log(longString.endsWith("ble.", 5));
+console.log(longString.includes("ble.", 10));
+console.log(longString.indexOf("long", 16));
+document.body.innerHTML = longString.anchor("haha");
+console.log(longString.indexOf("long", -10));
+console.log(longString.indexOf("long", longString.length + 1));
+console.log(longString.lastIndexOf("long", -10));
+console.log(longString.lastIndexOf("long", longString.length + 1));
+document.write(hottext.link(url));
+console.log(longString.normalize("NFD"));
 
-var s = new String("111");
-console.log(s.prototype === String.prototype);
+console.log(longString.slice(-10, -1));
+
+console.log(longString.slice(5, 14));
+console.log(longString.slice(-14, -1));
+console.log(longString.slice(15, 5));
+console.log(longString.substring(10, 20));
+console.log(longString.substring(-1, 20));
+console.log(longString.substring(10, -1));
+console.log(longString.substr(1,20));
+console.log(longString.substr(-10, 20));
+var sr = "                         adadada               ";
+console.log(sr);
+console.log(sr.trim());
+console.log(sr.trimLeft());
+console.log(sr.trimRight());
